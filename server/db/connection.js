41a60894f1fec833ef364dde67bd50868529626d
pappan-123 <85434109+pappan-123@users.noreply.github.com>
2024-01-20 00:50:52 +0530

    back

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.MONGODB_URL2, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => {
     console.log(`DB connection successful`);
